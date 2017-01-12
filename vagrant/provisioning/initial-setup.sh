e4f2f76490e37065a5073b1bd90a3f3202f79e97
Harry <Harryyhzhang@gmail.com>
2017-01-11 21:35:07 -0500

    add the environment variable

--- vagrant/provisioning/initial-setup.sh
+++ vagrant/provisioning/initial-setup.sh
@@ -1,10 +1,10 @@
 echo "### $1"
-sudo sh -c '(echo "Acquire::http::proxy \"http://10.100.33.50:8080/\";";  echo "Acquire::ftp::proxy \"ftp://10.100.33.50:8080/\";";  echo "Acquire::https::proxy \"https://10.100.33.50:8080/\";") >  /etc/apt/apt.conf'
+#sudo sh -c '(echo "Acquire::http::proxy \"http://10.100.33.50:8080/\";";  echo "Acquire::ftp::proxy \"ftp://10.100.33.50:8080/\";";  echo "Acquire::https::proxy \"https://10.100.33.50:8080/\";") >  /etc/apt/apt.conf'
 # although proxy was set in etc/apt/apt.conf, but for some packages which is downloaded through http, it still need to setup proxy envrionment variable
-export http_proxy="http://10.100.33.50:8080"
-export https_proxy="http://10.100.33.50:8080"
+#export http_proxy="http://10.100.33.50:8080"
+#export https_proxy="http://10.100.33.50:8080"
 
-#sudo apt-get update
+sudo apt-get update
 #sudo apt-get -y install default-jdk
 
 sudo addgroup hadoop
@@ -23,8 +23,36 @@ chmod 600 .ssh/id_rsa
 cat .ssh/id_rsa.pub >> .ssh/authorized_keys
 
 
-ssh-keyscan -H 192.168.77.21 >> .ssh/known_hosts
-ssh-keyscan -H 192.168.77.22 >> .ssh/known_hosts
+#ssh-keyscan -H 192.168.77.21 >> .ssh/known_hosts
+#ssh-keyscan -H 192.168.77.22 >> .ssh/known_hosts
 
 sudo chown -R hduser:hadoop .ssh
 chmod 700 .ssh
+
+cd ~
+sudo apt-get install default-jdk
+export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64/
+wget http://mirrors.sonic.net/apache/hadoop/common/hadoop-2.7.1/hadoop-2.7.1.tar.gz
+
+tar xvzf hadoop-2.7.1.tar.gz
+
+sudo mv hadoop-2.7.1 /usr/local/hadoop
+sudo chown -R hduser:hadoop /usr/local/hadoop/
+
+echo '
+export JAVA_HOME=/usr/lib/jvm/java-7-openjdk-amd64
+export HADOOP_HOME=/usr/local/hadoop
+export PATH=$PATH:$HADOOP_HOME/bin
+export PATH=$PATH:$HADOOP_HOME/sbin
+export HADOOP_MAPRED_HOME=$HADOOP_HOME
+export HADOOP_COMMON_HOME=$HADOOP_HOME
+export HADOOP_HDFS_HOME=$HADOOP_HOME
+export YARN_HOME=$HADOOP_HOME
+export HADOOP_COMMON_LIB_NATIVE_DIR=$HADOOP_HOME/lib/native
+export HADOOP_OPTS="-D java.library.path=$HADOOP_HOME/lib"
+' >> /home/hduser/.bashrc
+
+#export JAVA_HOME=/usr/lib/jvm/java-7-openjdk-amd64
+#into /usr/local/hadoop/etc/hadoop/hadoop-env.sh
+
+#core-site;hdfs-site;yarn-site;mapred-site
