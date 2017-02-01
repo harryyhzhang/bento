816226fb572124e2a4dafa97d3170e4a04055a63
Harry <Harryyhzhang@gmail.com>
2017-01-31 22:16:55 -0500

    fix issue that failed to run

--- vagrant/provisioning2/runcalculate.sh
+++ vagrant/provisioning2/runcalculate.sh
@@ -5,12 +5,12 @@ rm -rf ~/out/*
 cp /usr/local/hadoop/etc/hadoop/*.xml ~/input/
  
  
-hadoop dfs -mkdir /usr
-hadoop dfs -mkdir /usr/in
-hadoop dfs -put ~/input/* /usr/in
-hadoop jar /usr/local/hadoop/share/hadoop/mapreduce/hadoop-mapreduce-examples-2.7.1.jar  grep /usr/in /usr/output 'dfs[a-z]+'
+/usr/local/hadoop/bin/hadoop dfs -mkdir /usr
+/usr/local/hadoop/bin/hadoop dfs -mkdir /usr/in
+/usr/local/hadoop/bin/hadoop dfs -put ~/input/* /usr/in
+/usr/local/hadoop/bin/hadoop jar /usr/local/hadoop/share/hadoop/mapreduce/hadoop-mapreduce-examples-2.7.1.jar  grep /usr/in /usr/output 'dfs[a-z]+'
 
 
-hadoop dfs -get /usr/output/* ~/out/
+/usr/local/hadoop/bin/hadoop dfs -get /usr/output/* ~/out/
  
 
